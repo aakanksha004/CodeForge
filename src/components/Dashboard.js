6da357260c21f253dd0f5bb44b2eb1546a02ec36
aakanksha004 <111703460+aakanksha004@users.noreply.github.com>
2024-12-05 21:27:09 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -247,7 +247,7 @@ const Dashboard = () => {
 
           <button
   onClick={toggleTagFilter}
-  className="relative group overflow-hidden text-white p-3 rounded-3xl flex items-center gap-2 bg-gradient-to-r from-purple-600 via-indigo-700 to-indigo-900"
+  className="relative group overflow-hidden text-white p-3 rounded-3xl flex items-center gap-2 bg-gradient-to-r from-purple-400 via-indigo-500 to-indigo-600"
 >
   <div className="absolute inset-0 w-0 bg-gradient-to-r from-indigo-900 via-purple-600 to-indigo-700 transition-all duration-500 ease-out group-hover:w-full" />
   <span className="flex items-center gap-2 relative z-10">
@@ -270,7 +270,7 @@ const Dashboard = () => {
           <button
         onClick={toggleStarredFilter}
         className={`relative group overflow-hidden text-white p-3 rounded-3xl flex items-center gap-2 ${
-          showStarred ? 'bg-gradient-to-r from-purple-600 via-indigo-700 to-indigo-900' : 'bg-gradient-to-r from-purple-600 via-indigo-700 to-indigo-900'
+          showStarred ? 'bg-gradient-to-r from-purple-400 via-indigo-500 to-indigo-600' : 'bg-gradient-to-r from-purple-400 via-indigo-500 to-indigo-600'
         }`}
       >
         <div className="absolute inset-0 w-0 bg-gradient-to-r from-indigo-900 via-purple-600 to-indigo-700 transition-all duration-500 ease-out group-hover:w-full" />
@@ -285,7 +285,7 @@ const Dashboard = () => {
         onClick={handleRefresh}
         className="relative group overflow-hidden text-white p-3 rounded-3xl"
       >
-        <div className="absolute inset-0 bg-gradient-to-r from-purple-600 via-indigo-700 to-indigo-900" />
+        <div className="absolute inset-0 bg-gradient-to-r from-purple-400 via-indigo-500 to-indigo-600" />
         <div className="absolute inset-0 w-0 bg-gradient-to-r from-indigo-900 via-purple-600 to-indigo-700 transition-all duration-500 ease-out group-hover:w-full" />
         <span className="flex items-center relative z-10">
           <FaSync className="w-4 h-4" />
